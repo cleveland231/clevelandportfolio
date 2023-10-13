f504a5c245cf25e14ec2ad5d395087f42b8fe99a
Cleveland Ticoalu <ayocleveland@gmail.com>
2023-10-13 14:53:54 -0600

    Add media

--- src/Components/Media/Media.js
+++ src/Components/Media/Media.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Media.css';
+import LongsPeak from '../../Assets/longspeak.jpeg'
+import StMartin from '../../Assets/stmartin.jpeg'
+import Utah from '../../Assets/utah.jpeg'
 
 export const Media = () => {
   return (
     <div className='Media'>
-      <div>
-        Coming soon...
+      <h2> Media </h2>
+      <div className='PhotoBoxHolder'>
+        <img className='MediaPic' src={ LongsPeak } alt="Longs Peak" />
+        <img className='MediaPic' src={ StMartin } alt="Thurgood by da beach" />
+        <img className='MediaPic' src={ Utah } alt="Utah" />
       </div>
 
     </div>
